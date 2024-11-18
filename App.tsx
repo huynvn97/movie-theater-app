f9650c2fe20c9a45d78e8a66ab058ae05c40dda8
Jackson.Nguyen <huynvn97@gmail.com>
2024-11-19 00:24:23 +0700

    chore: config redux provider

--- App.tsx
+++ App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
 import RootNavigation from './src/navigation/RootNavigation';
+import {Provider} from 'react-redux';
+import {stateManagerStore} from 'movie-theater-sdk';
 
 function App(): React.JSX.Element {
-  return <RootNavigation />;
+  return (
+    <Provider store={stateManagerStore}>
+      <RootNavigation />
+    </Provider>
+  );
 }
 
 export default App;
