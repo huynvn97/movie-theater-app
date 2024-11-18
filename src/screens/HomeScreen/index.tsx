eb45564dad14508b1b99600f210adc13f05955db
Jackson.Nguyen <huynvn97@gmail.com>
2024-11-19 00:57:10 +0700

    feat(homeScreen): show basic list movies

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,10 +1,71 @@
-import React from 'react';
-import {ScrollView, Text} from 'react-native';
+import React, {useCallback, useState} from 'react';
+import {View, StyleSheet, TextInput, FlatList, Text} from 'react-native';
+import {Movie, useGetMovies} from 'movie-theater-sdk';
+import useListItemHeight from '../../hooks/useListItemHeight';
 
 export default function HomeScreen() {
+  const [searchQuery, setSearchQuery] = useState('');
+  const {movies} = useGetMovies();
+
+  const {listHeight, ref} = useListItemHeight();
+
+  const renderItem = useCallback(
+    ({item}: {item: Movie}) => {
+      return (
+        <View style={[styles.item, {height: listHeight}]}>
+          <Text style={styles.title}>{item.title}</Text>
+          <Text style={styles.channelTitle} numberOfLines={2}>
+            {item.overview}
+          </Text>
+        </View>
+      );
+    },
+    [listHeight],
+  );
+
   return (
-    <ScrollView>
-      <Text>homeScreen</Text>
-    </ScrollView>
+    <View style={[styles.container]}>
+      <TextInput
+        style={styles.searchBar}
+        placeholder="Search movies..."
+        value={searchQuery}
+        onChangeText={text => setSearchQuery(text)}
+      />
+      <View style={{flex: 1}} ref={ref}>
+        <FlatList
+          data={movies}
+          renderItem={renderItem}
+          initialNumToRender={10}
+          windowSize={21}
+          maxToRenderPerBatch={10}
+          showsVerticalScrollIndicator={false}
+        />
+      </View>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#f8f9fa',
+  },
+  searchBar: {
+    width: '100%',
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 8,
+    paddingHorizontal: 10,
+    marginBottom: 10,
+  },
+  item: {
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  channelTitle: {},
+});
