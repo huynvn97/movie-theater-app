653a8138e2ad3466f880caf829729667ebcd566f
Jackson.Nguyen <huynvn97@gmail.com>
2024-11-19 16:09:04 +0700

    chore: handle init sdk

--- App.tsx
+++ App.tsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 import RootNavigation from './src/navigation/RootNavigation';
 import {Provider} from 'react-redux';
-import {stateManagerStore} from 'movie-theater-sdk';
+import {httpClient, init, stateManagerStore} from 'movie-theater-sdk';
+import {API_BASE_URL} from './src/utils/constants';
+
+/**
+ * * Config SDK
+ */
+init({baseURL: API_BASE_URL});
+httpClient.interceptors.request.use(config => {
+  // TODO: in real project, we should get token from storage
+  // TODO: in real project, BASE_URL should be from .env
+  config.headers.Authorization =
+    'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkOTgyYzUwNWQ4MzI0MDQxN2Q0NzY2MzhmNmQ0NjY3NyIsIm5iZiI6MTczMTk0NzM3NC4wMjU5NjA0LCJzdWIiOiI2NzNiNmIwMjZhMDJhMjRkN2IyMWFmMzUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.Sx54NFz2JLToJ23OouVM9kco8vcUXz9ElgHwHr5vZos';
+  return config;
+});
 
 function App(): React.JSX.Element {
   return (
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,2 @@
 export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
+export const API_BASE_URL = 'https://api.themoviedb.org/3';
