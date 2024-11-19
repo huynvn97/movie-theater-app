3d99288a0ea0b1fe5fc6cf2944f03776722a9c4f
Jackson.Nguyen <huynvn97@gmail.com>
2024-11-19 11:21:23 +0700

    style(MovieCard): show posters

--- src/components/MovieCard/index.tsx
+++ src/components/MovieCard/index.tsx
@@ -1,5 +1,13 @@
 import {Movie} from 'movie-theater-sdk';
-import {StyleProp, StyleSheet, Text, View, ViewStyle} from 'react-native';
+import {
+  ImageBackground,
+  StyleProp,
+  StyleSheet,
+  Text,
+  View,
+  ViewStyle,
+} from 'react-native';
+import {IMAGE_BASE_URL} from '../../utils/constants';
 
 type MovieCardProps = {
   item: Movie;
@@ -11,6 +19,15 @@ export default function MovieCard(props: MovieCardProps) {
 
   return (
     <View style={[styles.item, StyleSheet.flatten(containerStyle)]}>
+      <ImageBackground
+        source={{
+          uri: `${IMAGE_BASE_URL}/w500/${item.backdrop_path}`,
+        }}
+        style={styles.imgBackground}
+        resizeMethod="auto"
+        resizeMode="cover">
+        <View style={styles.imgWrapper} />
+      </ImageBackground>
       <Text style={styles.title}>{item.title}</Text>
       <Text style={styles.channelTitle} numberOfLines={2}>
         {item.overview}
@@ -23,10 +40,26 @@ const styles = StyleSheet.create({
   item: {
     backgroundColor: '#fff',
     padding: 8,
+    borderRadius: 8,
+    overflow: 'hidden',
   },
   title: {
     fontSize: 16,
     fontWeight: 'bold',
+    color: 'white',
   },
-  channelTitle: {},
+  channelTitle: {
+    color: 'white',
+  },
+  imgBackground: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+  },
+  imgWrapper: {
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.5)',
+  },
 });
